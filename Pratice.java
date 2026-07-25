1c0614c09c1c0c4c544f68852624ac54cd462a7b
YOCODER7 <yuvrajsinhrajput1576@gmail.com>
2026-07-25 18:48:23 +0530

    Add files via upload

---
+++ Pratice.java
@@ -0,0 +1,118 @@
+class pratice {
+
+    static class Node {
+        int data;
+        Node next;
+        Node prev;
+
+        Node(int value) {
+            data = value;
+            next = null;
+            prev = null;
+        }
+    }
+
+    Node head = null;
+
+    void insert(int value) {
+
+        Node newNode = new Node(value);
+        newNode.next = head;
+
+        if (head != null) {
+            head.prev = newNode;
+        }
+
+        head = newNode;
+        System.out.println(value + " Inserted ");
+    }
+
+    public void delete(int value) {
+
+        if (head == null) {
+            System.out.println("List is empty.");
+            return;
+        }
+
+        Node temp = head;
+
+       
+        if (head.data == value) {
+            head = head.next;
+            if (head != null) {
+                head.prev = null;
+            }
+            System.out.println(value + " Deleted ");
+            return;
+        }
+
+       
+        while (temp != null && temp.data != value) {
+            temp = temp.next;
+        }
+
+        if (temp == null) {
+            System.out.println("Value not found.");
+            return;
+        }
+
+         
+        if (temp.next != null) {
+            temp.next.prev = temp.prev;
+        }
+
+        if (temp.prev != null) {
+            temp.prev.next = temp.next;
+        }
+
+        System.out.println(value + " Deleted ");
+    }
+
+    public void display() {
+
+    if (head == null) {
+        System.out.println("null");
+        return;
+    }
+
+    Node temp = head;
+
+    System.out.print("null <-> ");
+
+    while (temp != null) {
+        System.out.print(temp.data + " <-> ");
+        temp = temp.next;
+    }
+
+    System.out.println("null");
+}
+
+
+    public static void main(String[] args) {
+
+        pratice list = new pratice();
+
+
+        list.insert(1000);
+        list.insert(2000);
+        list.insert(300);
+
+        list.display();
+
+        list.delete(2000);
+        list.display();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+    
